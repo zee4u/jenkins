4d58320add274c7141d7018b2650c4e523e2c030
zee4u <48729563+zee4u@users.noreply.github.com>
2024-05-31 03:09:20 +0500

    Add files via upload

---
+++ main.java
@@ -0,0 +1,18 @@
+
+import java.util.Date;
+
+
+public class main{
+public static void main(String[] args)
+{
+System.out.println("This is Java Program");
+System.out.println("Output from  Java Program");
+System.out.println(new Date());
+
+
+
+}
+
+
+
+}
