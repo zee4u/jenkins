58467b2aa95197ea30352875f8ab7f710712fc6f
zee4u <48729563+zee4u@users.noreply.github.com>
2024-05-31 15:59:00 +0500

    Sample

--- main.java
+++ main.java
@@ -6,10 +6,8 @@ public class main{
 public static void main(String[] args)
 {
 System.out.println("This is Java Program");
-System.out.println("Output from  Java Program");
-System.out.println(new Date());
-  System.out.println("This is Chawal Program");
-System.out.println("Output from  Zentakouf Program");
+System.out.println("CI/CD Implementation");
+
 
 
 
